1a4de299ecccfb8c209043502c68f78b43895004
Abd Elrahman Mohamed <abdulmoha@outlook.it>
2024-07-21 14:47:37 +0200

    gg

--- frontEnd/src/services/Api.js
+++ frontEnd/src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://project-mern-3pf8.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
 const api = axios.create({baseURL: API_URL});
 
 // NEW! Aggiungi un interceptor per includere il token in tutte le richieste
