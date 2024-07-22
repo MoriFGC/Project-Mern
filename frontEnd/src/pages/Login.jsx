da1063f6ff84147737bc277c731731ba605122f0
Abd Elrahman Mohamed <abdulmoha@outlook.it>
2024-07-22 16:36:02 +0200

    gg

--- frontEnd/src/pages/Login.jsx
+++ frontEnd/src/pages/Login.jsx
@@ -55,9 +55,6 @@ export default function Login() {
     window.location.href = `${API_URL}/api/auth/github`;
   };
 
-  if (isLoggedIn) {
-    return null;
-  }
 
   return (
     <div className="min-h-screen flex items-center justify-center px-4 sm:px-6 lg:px-8">
